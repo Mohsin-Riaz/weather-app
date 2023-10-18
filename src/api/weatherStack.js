790bb9867e929bb18281a7dd7b4fe1c9a96a8baf
Mohsin Riaz <riaz.mohsin@gmail.com>
2023-10-18 09:21:30 -0400

    Modified: code cleanup

--- src/api/weatherStack.js
+++ src/api/weatherStack.js
@@ -3,17 +3,16 @@ const baseURL = 'http://api.weatherstack.com';
 
 const api_key = { access_key: import.meta.env.VITE_WEATHERSTACK_TOKEN };
 
+console.log(api_key);
+
 const getCurrentWeather = async ([long, lat]) => {
-    console.log([long, lat]);
     try {
         const response = await axios.get(`${baseURL}/current`, {
             params: {
                 ...api_key,
                 query: `${lat.toPrecision(8)},${long.toPrecision(8)}`,
-                // query: 'Mississauga',
             },
         });
-        console.log(response.data);
         if (response.status === 200) return response.data;
     } catch (error) {
         console.log(error);
