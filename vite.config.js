0dd808cc2567a73060d2cf368a634fe1d80d2212
Mohsin Riaz <riaz.mohsin@gmail.com>
2023-10-18 09:00:39 -0400

    added: gh-pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import { defineConfig } from 'vite';
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    server: { port: 3000, host: true, open: true },
+    server: { port: 3000, open: true, host: true },
+    base: '/weather-app/',
 });
