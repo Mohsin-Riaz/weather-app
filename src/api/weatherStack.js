e6853c355829f0b3c8f1033911baeca5812e032c
Mohsin Riaz <riaz.mohsin@gmail.com>
2024-05-01 00:49:16 -0400

    Added: secret key

--- src/api/weatherStack.js
+++ src/api/weatherStack.js
@@ -1,29 +1,30 @@
 import axios from 'axios';
-// const apiKey = import.meta.env.VITE_WEATHER_API_TOKEN;
+const apiKey = import.meta.env.VITE_WEATHER_API_TOKEN;
 const weatherApiUrl = import.meta.env.VITE_WEATHER_URL;
 
 const getCurrentWeather = async ([long, lat]) => {
-    try {
-        await axios({
-            method: 'GET',
-            url: weatherApiUrl + '/?' + `${lat},${long}`,
-        });
-    } catch (error) {
-        console.log(error);
-    }
-
+    // if (!weatherApiUrl) return new Error('No api connected');
     // try {
-    //     const response = await axios({
+    //     await axios({
     //         method: 'GET',
-    //         url:
-    //             'https://api.weatherapi.com/v1/current.json' +
-    //             `?key=${apiKey}&q=${lat},${long}` +
-    //             '&aqi=no',
+    //         url: weatherApiUrl + '/?' + `${lat},${long}`,
     //     });
-    //     if (response.status === 200) return response.data;
     // } catch (error) {
     //     console.log(error);
     // }
+
+    try {
+        const response = await axios({
+            method: 'GET',
+            url:
+                'https://api.weatherapi.com/v1/current.json' +
+                `?key=${apiKey}&q=${lat},${long}` +
+                '&aqi=no',
+        });
+        if (response.status === 200) return response.data;
+    } catch (error) {
+        console.log(error);
+    }
 };
 
 export default getCurrentWeather;
